d8a33bea946a14e0e04a7763fd21a38fe1985576
Opilane TTHK <opilane@tthk.ee>
2022-01-12 13:14:04 +0200

    Add routing, models and controller

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+const mainRoute = require('./routes/mainRoute');
 
 const app = express();
+
+require('./models/db') // connect to db
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+
+// initialize session
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connection to database
+//mongoose.connect('mongodb://localhost:27017/mySuperSecretDB', {useUnifiedTopology: true});
+
+app.use(mainRoute);
 
 const port = 3000;
 
